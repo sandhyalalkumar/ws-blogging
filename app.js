363eec52ec5eb736b067b807943f46e63db169ab
Sandhyalal kumar <sandhyalalkumar@gmail.com>
2018-05-22 18:13:24 +0530

    Added debug

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const chalk = require("chalk");
-const debug = require("debug");
+const debug = require("debug")("ws-blogging");
+const bodyParser = require("body-parser");
+const config = require("config");
+const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 9000;
+const dbConfig = config.get("dbConfig");
+
+mongoose.connect(dbConfig.mongoUri)
+    .then(function(){
+    console.log("Successfully connected to the database");    
+    }).catch(function(err){
+        console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./blogger/routes/blog.routes")(app);
 
 app.listen(port, function(){
-    debug(`listening on port ${chalk.green(port)}`);
+    debug(`Server listening on port ${chalk.blue(port)}`);
 });
